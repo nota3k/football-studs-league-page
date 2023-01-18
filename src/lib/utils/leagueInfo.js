3690a7bfb9b35c929588c2296459ec2880312799
dopiedopeman <120254237+dopiedopeman@users.noreply.github.com>
2023-01-18 14:27:34 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "813468710336581632"; // your league ID
 export const leagueName = "Jeden Verdammten Sonntag"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 50; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
